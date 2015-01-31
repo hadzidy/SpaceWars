e2dd0c98e3de09f6232d3251d0aa7c7f735ab8d4
Hadzidy Recilla <hadzidy@grupow.com>
2015-01-30 20:37:47 -0600

    1 Asteroide Funciona!!! :D

--- app/scripts/com/spacewarsts/SpaceWarsGame.ts
+++ app/scripts/com/spacewarsts/SpaceWarsGame.ts
@@ -1,5 +1,6 @@
 /// <reference path="./display/SpaceShip.ts" />
 /// <reference path="./display/Bullets.ts" />
+/// <reference path="./display/Asteroids.ts" />
 /// <reference path="./ui/Keyboard.ts" />
 
 
@@ -7,6 +8,7 @@ module com.spacewarsts {
 
     import Spaceship= display.Spaceship;
     import Bullet= display.Bullets;
+    import Asteroids= display.Asteroids;
     import Keyboard = ui.Keyboard;
 
 
@@ -20,17 +22,29 @@ module com.spacewarsts {
             console.log("New Space Wars Game Created");
             Keyboard.initialize($(document));
         }
-
         public start(): void {
 
             console.log("Space Wars Game Started");
             this.stage = new createjs.Stage("GameCanvas");
+
+            var heightStage = this.stage.canvas.clientHeight;
+            var widthStage = this.stage.canvas.clientWidth;
+
+            var stageSize = {w: widthStage, h: heightStage};
+            
             this.ship = new Spaceship();
             this.stage.addChild(this.ship);
+
+            var asteroids = new Asteroids(stageSize);
+
+            this.stage.addChild(asteroids);
 
              var tick= (event)=> {
                  var deltaTime = event.delta;
                  this.ship.update(deltaTime);
+                 asteroids.update();
+
+
                  this.stage.update();
             }
             createjs.Ticker.setFPS(60)
