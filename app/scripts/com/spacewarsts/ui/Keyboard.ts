e8e342ffdf795313d64f1813537a2f4a9510cf58
Hadzidy Recilla <hadzidy@grupow.com>
2015-02-09 18:19:13 -0600

    keyboard fix

--- app/scripts/com/spacewarsts/ui/Keyboard.ts
+++ app/scripts/com/spacewarsts/ui/Keyboard.ts
@@ -8,28 +8,33 @@ module com.spacewarsts.ui {
     export class Keyboard {
 
         static UP_KEY   : number = 38;
-        static DOWN_KEY   : number = 40;
         static RIGHT_KEY   : number = 39;
         static LEFT_KEY   : number = 37;
         static SHOOT_KEY   : number = 32;
 
         private static CURRENT_KEY:number = -1;
-        
+        private static KEY_PRESS:Object = {};
+
         public static initialize(document:JQuery):void {
+
+            Keyboard.KEY_PRESS[Keyboard.UP_KEY] = false;
+            Keyboard.KEY_PRESS[Keyboard.RIGHT_KEY] = false;
+            Keyboard.KEY_PRESS[Keyboard.LEFT_KEY] = false;
+            Keyboard.KEY_PRESS[Keyboard.SHOOT_KEY] = false;
 
             document.keydown((e) => {
                 e.preventDefault();
-                Keyboard.CURRENT_KEY = e.keyCode;
+                Keyboard.KEY_PRESS[e.keyCode] = true;
             });
 
             document.keyup((e) => {
                 e.preventDefault();
-                Keyboard.CURRENT_KEY = -1;
+                Keyboard.KEY_PRESS[e.keyCode] = false;
             });
         }
 
         public static isKeyDown(pressKey:number):boolean {
-            return Keyboard.CURRENT_KEY == pressKey;
+            return Keyboard.KEY_PRESS[pressKey];
         }
     }
 }
